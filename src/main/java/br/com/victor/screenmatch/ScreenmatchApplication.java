d1398967c9286e097430e445e330dccf5231f214
Victor <vbatista.dev@gmail.com>
2025-06-23 11:43:27 -0300

    Construindo a requisição da API

--- src/main/java/br/com/victor/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/victor/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.victor.screenmatch;
 
+import br.com.victor.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem WEB");
+		String serie;
+
+		serie = "gilmore+girl";
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=" + serie + "&Season&apikey=");
 	}
 }
