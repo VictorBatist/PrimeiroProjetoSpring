14332b12b286183d1117c6c76f11ea734bfb111b
Victor <vbatista.dev@gmail.com>
2025-06-30 14:45:30 -0300

    Adicionando lista de temporadas

--- src/main/java/br/com/victor/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/victor/screenmatch/ScreenmatchApplication.java
@@ -2,12 +2,16 @@ package br.com.victor.screenmatch;
 
 import br.com.victor.screenmatch.model.DadosEpisodio;
 import br.com.victor.screenmatch.model.DadosSerie;
+import br.com.victor.screenmatch.model.DadosTemporada;
 import br.com.victor.screenmatch.requestApi.RequestApiKey;
 import br.com.victor.screenmatch.service.ConsumoApi;
 import br.com.victor.screenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -36,5 +40,14 @@ public class ScreenmatchApplication implements CommandLineRunner, RequestApiKey 
 		json =consumoApi.obterDados("https://www.omdbapi.com/?t=" + serie + "&season=1&episode=1&apikey=" + RequestApiKey.getApiKey());
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json,DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dadosSerie.totalTemporadas(); i++){
+			json =consumoApi.obterDados("https://www.omdbapi.com/?t=" + serie + "&season=" + i + "&apikey=" + RequestApiKey.getApiKey());
+			DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
